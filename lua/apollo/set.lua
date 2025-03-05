6540ff8f5e0860f0ebf9c630684d491e593eb98a
Apollo478 <mohamedbenhadjlakhal@gmail.com>
2025-03-05 22:08:45 +0100

    FEAT: added diagnostic text config

--- lua/apollo/set.lua
+++ lua/apollo/set.lua
@@ -8,7 +8,7 @@ vim.opt.expandtab = true
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
-
+vim.opt.guicursor = "n-v-i-c:block-Cursor"
 vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
@@ -18,4 +18,5 @@ vim.opt.incsearch=true
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
+vim.diagnostic.config({ virtual_text = true })
 
